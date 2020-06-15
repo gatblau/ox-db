6cdc2778071fb58b6d9a0f605d6c08f9f9cbaf90
gatblau <onix@gatblau.org>
2020-06-15 18:32:56 +0100

    upfdate manifest.json

---
+++ v4/dropfxs.sql
@@ -0,0 +1,19 @@
+DO
+$$
+    DECLARE
+        rec RECORD;
+        DROP_STATEMENT VARCHAR(200);
+    BEGIN
+        -- drops all functions that start with the FX_PATTERN prefix
+        FOR rec IN
+            SELECT routines.routine_name as fx_name
+            FROM information_schema.routines
+            WHERE routines.specific_schema='public'
+            AND routines.routine_name SIMILAR TO '<FX_PATTERN>'
+            ORDER BY routines.routine_name
+        LOOP
+            DROP_STATEMENT = 'DROP FUNCTION IF EXISTS ' || rec.fx_name || ' CASCADE;';
+            EXECUTE DROP_STATEMENT;
+        END LOOP;
+    END;
+$$
