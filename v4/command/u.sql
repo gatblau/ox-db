a078337fb02e16c9bafbcf7da274424a2b7aaeba
gatblau <onix@gatblau.org>
2021-07-31 13:13:22 +0100

    fix rule id

--- v4/command/u.sql
+++ v4/command/u.sql
@@ -95,7 +95,7 @@ VALUES (8, 'U_AREA_TO_LOCATION', 'Area -> Location Rule',
 -- Org -> Location
 INSERT INTO link_rule(id, key, name, description, link_type_id, start_item_type_id, end_item_type_id, version,
                       changed_by)
-VALUES (8, 'U_ORG_TO_LOCATION', 'Organisation -> Location Rule',
+VALUES (9, 'U_ORG_TO_LOCATION', 'Organisation -> Location Rule',
         'Connects an organisation to a location within it.', 1, 8, 9, 1, 'onix');
 
 INSERT INTO type_attribute(key, name, description, type, required, item_type_id, changed_by)
