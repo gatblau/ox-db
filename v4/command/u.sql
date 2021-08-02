ef15a77b648e5c1b23741d9520e2423d8d9874a4
gatblau <onix@gatblau.org>
2021-08-02 14:54:11 +0100

    fix id of area item type in link rule

--- v4/command/u.sql
+++ v4/command/u.sql
@@ -90,7 +90,7 @@ VALUES (7, 'U_ORG_GROUP_TO_AREA', 'Organization Group -> Area Rule',
 INSERT INTO link_rule(id, key, name, description, link_type_id, start_item_type_id, end_item_type_id, version,
                       changed_by)
 VALUES (8, 'U_AREA_TO_LOCATION', 'Area -> Location Rule',
-        'Connects an area to a location within it.', 1, 8, 10, 1, 'onix');
+        'Connects an area to a location within it.', 1, 9, 10, 1, 'onix');
 
 -- Org -> Location
 INSERT INTO link_rule(id, key, name, description, link_type_id, start_item_type_id, end_item_type_id, version,
